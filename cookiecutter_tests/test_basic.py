f6eb981d6f6652b100a47e74b3659aa2206166d2
Reinout van Rees <reinout@vanrees.org>
2016-10-20 13:59:44 +0200

    Demonstrating jinja template escape problem

--- cookiecutter_tests/test_basic.py
+++ cookiecutter_tests/test_basic.py
@@ -1,7 +1,29 @@
+from cookiecutter.main import cookiecutter
+from cookiecutter.utils import rmtree
 from unittest import TestCase
+
+import os
+import tempfile
 
 
 class BasicTest(TestCase):
 
-    def test_dummy(self):
-        self.assertEquals(1, 1)
+    def setUp(self):
+        self.temp_dir = tempfile.mkdtemp(suffix='cookiecuttertest')
+        self.our_dir = os.getcwd()
+        os.chdir(self.temp_dir)
+        self.defaults = {
+            "full_name": "Reinout van Rees",
+            "email": "reinout@vanrees.org",
+            "project_slug": "world-domination",
+            "project_short_description": "Speaks for itself"
+        }
+
+    def tearDown(self):
+        rmtree(self.temp_dir)
+        os.chdir(self.our_dir)
+
+    def test_smoke(self):
+        cookiecutter(self.our_dir,
+                     no_input=True,
+                     extra_context=self.defaults)
