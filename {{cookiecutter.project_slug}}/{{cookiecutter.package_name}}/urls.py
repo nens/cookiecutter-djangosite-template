bedd99c066b5d7cbaaa04c35b8c85650f520d397
Reinout van Rees <reinout@vanrees.org>
2018-02-09 11:27:32 +0100

    Removed now-unneeded 'include'

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/urls.py
@@ -5,14 +5,13 @@ from __future__ import print_function
 
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import include
 from django.urls import path
 
 from {{ cookiecutter.package_name }} import views
 
 
 urlpatterns = [
-    path(r'admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
     # path('something/',
     #     views.some_method,
     #     name="name_it"),
